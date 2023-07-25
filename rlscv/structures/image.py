97c9810c1de75f1bfcf6eff8098b60cc48ab30ac
markmno <nosanmark@gmail.com>
2023-07-25 14:35:40 +0300

    everything wxcept the tests works fine

--- rlscv/structures/image.py
+++ rlscv/structures/image.py
@@ -1,11 +1,15 @@
-from enum import Enum
 import math
-from typing import Self
-import torch
-import numpy as np
-import torchvision.transforms.v2.functional as T
-import cv2
 import os
+from dataclasses import dataclass
+from enum import Enum
+from typing import Self
+
+import cv2
+import numpy as np
+import torch
+import torchvision.transforms.v2.functional as T
+from torch import Tensor
+from PIL import Image
 
 
 class ImageFormat(Enum):
@@ -14,40 +18,21 @@ class ImageFormat(Enum):
     BCHW = "BCHW"
 
 
-class RLSImage:
-    def __init__(
-        self, data: torch.Tensor, state: ImageFormat = ImageFormat.CHW
-    ) -> None:
-        super().__init__()
-        self._data = data
-        self._state = state
-
-    def clone(self):
-        cloned = self._data.clone()
-        cur_state = self._state
-        return RLSImage(cloned, cur_state)
-
-    def reshape(self, h: int, w: int) -> "RLSImage":
-        self._data = T.resize(self._data, [h, w])
-        return self
+class RLSImage(Tensor):
+    _state = ImageFormat.CHW
 
     def rotate(self, angle: float) -> None:
         # FIXME ROTATE FIX
-        h, w = self._data.shape[:2]
+        h, w = self.data.shape[:2]
         diagonal: int = int(math.sqrt(h ^ 2 + w ^ 2))
-        self._data = T.pad(self._data, [diagonal, diagonal])
-        self._data = T.rotate(
-            self._data, angle, T.InterpolationMode.BILINEAR, expand=True
-        )
+        self.data = T.pad(self.data, [diagonal, diagonal])
+        self.data = T.rotate(self, angle, T.InterpolationMode.BILINEAR, expand=True)
 
-    def hflip(self) -> None:
-        self._data = T.hflip(self._data)
+    def hflip_(self) -> None:
+        self.data = T.hflip(self.data)
 
-    def vflip(self) -> None:
-        self._data = T.vflip(self._data)
-
-    def __array__(self):
-        return self._data.detach().numpy()
+    def vflip_(self) -> None:
+        self.data = T.vflip(self.data)
 
     @classmethod
     def load(cls, path: str) -> Self:
@@ -55,62 +40,65 @@ class RLSImage:
         data = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
         return cls(torch.Tensor(np.array(data) / 255.0).permute(2, 0, 1))  # type: ignore
 
-    @property
-    def __call__(self) -> torch.Tensor:
-        return self._data
-
     def to_chw(self) -> "RLSImage":
         """
         Convert to [channels, height, width]
         """
-        if self._data is None:
+        if self.data is None:
             raise KeyError()
         if self._state == ImageFormat.HWC:
-            data = self._data.permute(1, 2, 0).clone()
+            data = self.data.permute(1, 2, 0).clone()
         elif self._state == ImageFormat.BCHW:
-            data = self._data.squeeze(0).clone()
+            data = self.data.squeeze(0).clone()
         else:
-            data = self._data.clone()
+            data = self.data.clone()
         state = ImageFormat.CHW
-        return RLSImage(data, state)
+        image = RLSImage(data)
+        image._state = state
+        return image
 
     def to_bchw(self) -> "RLSImage":
         """
         Convert to [batch, channels, height, width]
         """
-        if self._data is None:
+        if self.data is None:
             raise KeyError()
         if self._state == ImageFormat.CHW:
-            data = self._data.unsqueeze(0).clone()
+            data = self.data.unsqueeze(0).clone()
         elif self._state == ImageFormat.HWC:
-            data = self._data.permute(2, 0, 1).unsqueeze(0).clone()
+            data = self.data.permute(2, 0, 1).unsqueeze(0).clone()
         else:
-            data = self._data.clone()
+            data = self.data.clone()
         state = ImageFormat.BCHW
-        return RLSImage(data, state)
+        image = RLSImage(data)
+        image._state = state
+        return image
 
     def to_hwc(self) -> "RLSImage":
         """
         Convert to [height, width, channels]
         """
-        if self._data is None:
+        if self.data is None:
             raise KeyError()
         if self._state == ImageFormat.BCHW:
-            data = self._data.squeeze(0).clone()
+            data = self.data.squeeze(0).clone()
         elif self._state == ImageFormat.CHW:
-            data = self._data.permute(1, 2, 0).clone()
+            data = self.data.permute(1, 2, 0).clone()
         else:
-            data = self._data.clone()
+            data = self.data.clone()
         state = ImageFormat.HWC
-        return RLSImage(data, state)
+        image = RLSImage(data)
+        image._state = state
+        return image
 
     def to_numpy(self) -> np.ndarray:
-        # TODO add dtype
-        if isinstance(self._data, torch.Tensor):
-            self._data.detach().cpu().numpy()
-        return np.asarray(self._data)
+        if isinstance(self.data, torch.Tensor):
+            self.data.detach().cpu().numpy()
+        return np.asarray(self.data)
 
     def save(self, path: str) -> None:
         if not os.path.isdir(os.path.dirname(path)):
             os.mkdir(os.path.dirname(path))
-        cv2.imwrite(path, self.to_numpy())
+        img = Image.fromarray(self.to_numpy()[0])
+        img = img.convert("RGB")
+        img.save(path)
---
+++ tests/structures/test_image.py
@@ -0,0 +1,86 @@
+import numpy as np
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+from torchvision.transforms.functional import resize
+from rlscv.structures.image import ImageFormat, RLSImage
+
+
+def test_load():
+    # Test loading an image
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    tr = torch.Tensor()
+    assert isinstance(img, RLSImage)
+    assert img.shape == (3, 224, 224)
+    assert img.max() <= 1.0
+    assert img.min() >= 0.0
+
+
+def test_reshape():
+    # Test resizing an image
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img = resize(img, [100, 100])
+    assert img.shape == (3, 100, 100)
+
+
+def test_rotate():
+    # Test rotating an image
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img.rotate(90)
+    # assert img.shape == (3, 224, 224)
+
+
+def test_hflip():
+    # Test horizontal flipping of an image
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img.hflip_()
+    # assert np.array_equal(img.to_numpy(), np.fliplr(img.to_numpy()))
+
+
+def test_vflip():
+    # Test vertical flipping of an image
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img.vflip_()
+    # assert np.array_equal(img.detach().cpu().numpy(), np.flipud(img.to_numpy()))
+
+
+def test_to_chw():
+    # Test converting an image to CHW format
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img_chw = img.to_chw()
+    assert img_chw.shape == (3, 224, 224)
+    assert img_chw._state == ImageFormat.CHW
+
+
+def test_to_bchw():
+    # Test converting an image to BCHW format
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img_bchw = img.to_bchw()
+    assert img_bchw.shape == (1, 3, 224, 224)
+    assert img_bchw._state == ImageFormat.BCHW
+
+
+def test_to_hwc():
+    # Test converting an image to HWC format
+    img_path = "tests/test_data/test_image.jpg"
+    img = RLSImage.load(img_path)
+    img_hwc = img.to_hwc()
+    assert img_hwc.shape == (224, 224, 3)
+    assert img_hwc._state == ImageFormat.HWC
+
+
+# def test_save():
+#     # Test saving an image
+#     img_path = "tests/test_data/test_image.jpg"
+#     img = RLSImage.load(img_path)
+#     img.save("tests/test_data/test_save.jpg")
+#     img_saved = ToTensor()(Image.open("tests/test_data/test_save.jpg"))
+#     print(img.shape, img_saved.shape)
+#     assert np.array_equal(img_saved.numpy(), img.to_numpy())
